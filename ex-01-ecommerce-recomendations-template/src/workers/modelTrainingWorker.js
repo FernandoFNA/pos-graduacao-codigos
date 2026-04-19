a8ffc11dad3328f872a443f44ee64720217a22c4
Maffz <fernando_fna@hotmail.com>
2026-04-18 22:01:17 -0300

    Fim ex02

--- ex-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
+++ ex-01-ecommerce-recomendations-template/src/workers/modelTrainingWorker.js
@@ -3,6 +3,7 @@ import { workerEvents } from "../events/constants.js";
 
 console.log("Model training worker initialized");
 let _globalCtx = {};
+let _model = null;
 
 const WEIGHTS = {
   category: 0.4,
@@ -30,27 +31,120 @@ async function trainModel({ users }) {
     };
   });
 
-  debugger
   _globalCtx = context;
 
+  const trainData = createTrainingData(context);
+  _model = await configureAndTrainNeuralNet(trainData);
+
   postMessage({
-    type: workerEvents.trainingLog,
-    epoch: 1,
-    loss: 1,
-    accuracy: 1,
+    type: workerEvents.progressUpdate,
+    progress: { progress: 100 },
   });
 
-  setTimeout(() => {
-    postMessage({
-      type: workerEvents.progressUpdate,
-      progress: { progress: 100 },
-    });
-    postMessage({ type: workerEvents.trainingComplete });
-  }, 1000);
+  postMessage({ type: workerEvents.trainingComplete });
 }
 
-const oneHotWeighted = (index, lenght, weight) =>
-  tf.oneHot(index, lenght).cast("float32").mul(weight);
+async function configureAndTrainNeuralNet(trainData) {
+  const model = tf.sequential();
+
+  model.add(
+    tf.layers.dense({
+      inputShape: [trainData.inputDimention],
+      units: 128,
+      activation: "relu",
+    }),
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 64,
+      activation: "relu",
+    }),
+  );
+
+  model.add(
+    tf.layers.dense({
+      units: 32,
+      activation: "relu",
+    }),
+  );
+
+  model.add(tf.layers.dense({ units: 1, activation: "sigmoid" }));
+
+  model.compile({
+    optimizer: tf.train.adam(0.01),
+    loss: "binaryCrossentropy",
+    metrics: [`accuracy`],
+  });
+
+  await model.fit(trainData.xs, trainData.ys, {
+    epochs: 100,
+    batchSize: 32,
+    shuffle: true,
+    callbacks: {
+      onEpochEnd: (epoch, logs) => {
+        postMessage({
+          type: workerEvents.trainingLog,
+          epoch: epoch,
+          loss: logs.loss,
+          accuracy: logs.acc,
+        });
+      },
+    },
+  });
+
+  return model;
+}
+
+function createTrainingData(context) {
+  const inputs = [];
+  const labels = [];
+
+  context.users
+    .filter((u) => u.purchases.length)
+    .forEach((user) => {
+      const userVector = encodeUser(user, context).dataSync();
+      context.products.forEach((p) => {
+        const productVector = encodeProduct(p, context).dataSync();
+
+        const label = user.purchases.some((purchase) =>
+          purchase.name === p.name ? 1 : 0,
+        );
+
+        inputs.push([...userVector, ...productVector]);
+        labels.push(label);
+      });
+    });
+
+  return {
+    xs: tf.tensor2d(inputs),
+    ys: tf.tensor2d(labels, [labels.length, 1]),
+    inputDimention: context.dimensions * 2, //tamanho = userVector + productVector
+  };
+}
+
+function encodeUser(user, context) {
+  if (user.purchases.length) {
+    return tf
+      .stack(user.purchases.map((product) => encodeProduct(product, context)))
+      .mean(0)
+      .reshape([1, context.dimensions]);
+  }
+
+  return tf
+    .concat1d([
+      tf.zeros([1]), //ignorar preco (user sem compra)
+      tf.tensor1d([
+        normalize(user.age, context.minAge, context.maxAge) * WEIGHTS.age,
+      ]),
+      tf.zeros([context.numCategories]), //ignorar categoria (user sem compra)
+      tf.zeros([context.numColors]), //ignorar cor (user sem compra)
+    ])
+    .reshape([1, context.dimensions]);
+}
+
+const oneHotWeighted = (index, length, weight) =>
+  tf.oneHot(index, length).cast("float32").mul(weight);
 
 function encodeProduct(product, context) {
   const price = tf.tensor1d([
@@ -78,9 +172,9 @@ function encodeProduct(product, context) {
   return tf.concat1d([price, age, category, color]);
 }
 
-function makeContext(catalog, users) {
+function makeContext(products, users) {
   const ages = users.map((u) => u.age);
-  const prices = catalog.map((c) => c.price);
+  const prices = products.map((c) => c.price);
 
   const minAge = Math.min(...ages);
   const maxAge = Math.max(...ages);
@@ -88,8 +182,8 @@ function makeContext(catalog, users) {
   const minPrice = Math.min(...prices);
   const maxPrice = Math.max(...prices);
 
-  const colors = [...new Set(catalog.map((c) => c.color))];
-  const categories = [...new Set(catalog.map((c) => c.category))];
+  const colors = [...new Set(products.map((c) => c.color))];
+  const categories = [...new Set(products.map((c) => c.category))];
 
   const colorsIndex = Object.fromEntries(
     colors.map((color, index) => {
@@ -116,7 +210,7 @@ function makeContext(catalog, users) {
   });
 
   const avgAgeNormProducts = Object.fromEntries(
-    catalog.map((product) => {
+    products.map((product) => {
       const avg = ageCounts[product.name]
         ? ageSums[product.name] / ageCounts[product.name]
         : midAge;
@@ -126,7 +220,7 @@ function makeContext(catalog, users) {
   );
 
   return {
-    catalog,
+    products,
     users,
     colorsIndex,
     categoriesIndex,
@@ -145,12 +239,29 @@ function makeContext(catalog, users) {
 const normalize = (value, min, max) => (value - min) / (max - min || 1);
 
 function recommend(user, ctx) {
-  console.log("will recommend for user:", user);
-  // postMessage({
-  //     type: workerEvents.recommend,
-  //     user,
-  //     recommendations: []
-  // });
+  if (!_model) return;
+
+  const userVector = encodeUser(user, ctx).dataSync();
+
+  const inputs = ctx.productVectors.map(({ vector }) => {
+    return [...userVector, ...vector];
+  });
+
+  const inputTensor = tf.tensor2d(inputs);
+  const predictions = _model.predict(inputTensor);
+  const scores = predictions.dataSync();
+
+  const recommendations = ctx.productVectors.map((item, index) => {
+    return { ...item.meta, name: item.name, score: scores[index] };
+  });
+
+  const sortedItems = recommendations.sort((a, b) => b.score - a.score);
+
+  postMessage({
+    type: workerEvents.recommend,
+    user,
+    recommendations: sortedItems,
+  });
 }
 
 const handlers = {
